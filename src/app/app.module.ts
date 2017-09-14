99a0e1e73f69260f2ccc628fd500e1748e7707e4
LeandroVidal92 <leandrovidal92@gmail.com>
2017-09-13 23:04:19 -0300

    Login con firebase OK

---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvYNnOsPCinu79_TmuRpUGFLx1WRGRVeM",
+  authDomain: "usuariopps-965b9.firebaseapp.com",
+  databaseURL: "https://usuariopps-965b9.firebaseio.com",
+  projectId: "usuariopps-965b9",
+  storageBucket: "usuariopps-965b9.appspot.com",
+  messagingSenderId: "957900585632"
+};
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireAuthModule,AngularFireAuth, } from 'angularfire2/auth';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {firebase}  from 'firebase/database';
+import { TabsPage } from '../tabs/tabs';
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+username:string;
+password:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams,private _auth:AngularFireAuth) {
+  }
+  async login()
+  {
+  var result = await this._auth.auth.signInWithEmailAndPassword(this.username,this.password)
+                        .catch(function(error) {         
+                            alert(error.message);
+                            //console.log(error);
+                            
+                        });
+
+                      if(result!=undefined){
+                        this.navCtrl.push(TabsPage)
+                        console.log("INGRESO");
+                      }
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
